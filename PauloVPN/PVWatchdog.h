4a5a4c2d34fc843a1870e8609755745f7e1c6b64
pavsayganov-studio <pav.sayganov@gmail.com>
2026-05-21 08:40:04 +0000

    refactor: ultimate architecture overhaul enforcing strict clash routing rules and stable system proxy

--- PauloVPN/PVWatchdog.h
+++
@@ -1,6 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PVWatchdog : NSObject
-+ (instancetype)shared;
-- (void)startMonitoring:(NSTask *)task startSelector:(SEL)selector target:(id)target;
-- (void)stopMonitoring;
-@end
